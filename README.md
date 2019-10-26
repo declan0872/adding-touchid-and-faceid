8422ead18327333bbc61b1b63621bd7c21025cea
declan0873 <declan.c@hotmail.com>
2019-10-26 17:34:07 +0100

    Added new readme

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# adding-touchid-and-faceid
+# Adding Touch ID and Face ID to an app
+
+How to add Touch ID and Face ID to a Swift app for iOS 12 and iOS 13.
+
+## Tech Specs
+
+- Supports iOS 12 and iOS 13.
+- Swift 5
+- Storyboards
+
+<p>
+  <img align="left" style="padding: 10px;" src="images/image1.png" width="350" title="Image 1">
+  <img align="right" style="padding: 10px;" src="images/image2.png" width="350" title="Image 2">
+  <img align="right" style="padding: 10px;" src="images/image3.png" width="350" title="Image 3">
+  <img align="right" style="padding: 10px;" src="images/image4.png" width="350" title="Image 4">
+</p>
+
--- adding-touchid-and-faceid/ViewController.swift
+++ adding-touchid-and-faceid/ViewController.swift
@@ -6,15 +6,65 @@
 //  Copyright © 2019 Declan. All rights reserved.
 //
 
+//Please note: Privacy - Face ID Usage Description also added in info.plist
+
 import UIKit
+import LocalAuthentication
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var signInButton: UIButton!
+    @IBOutlet weak var signedInLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        signedInLabel.isHidden = true
     }
 
 
-}
+    @IBAction func signInButtonTapped(_ sender: Any) {
+                
+        let context = LAContext()
+        
+        var error: NSError?
+        
+        //Check if the device support authentication biometrics
+        if(context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error)) {
+            
+            //This message is only shown to users with touch ID
+            let reason = "Identify yourself!"
+            
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { [weak self] success, authenticationError in
+            
+            DispatchQueue.main.async {
+                    if success{
+                        print("Success - Unlocked")
+                        self?.signInButton.isHidden = true
+                        self?.signedInLabel.isHidden = false
+                    } else {
+                        //i.e issue with being authenticated
+                        print("Error - Locked")
+                        
+                        //Show alert error
+                        let ac = UIAlertController(title: "Authentication Failed", message: "You could not be verified; please try again.", preferredStyle: .alert)
+                        ac.addAction(UIAlertAction(title: "Ok", style: .default))
+                        self?.present(ac, animated: true)
+                        
+                    }
+                }
+            }
+        } else {
+            //No biometrics
+            
+            //Show alert error
+            let ac = UIAlertController(title: "Biometrics Unavailable", message: "Your device is not configured for biometric authentication", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Ok", style: .default))
+            present(ac, animated: true)
+            
+        }
+    }
 
+}
